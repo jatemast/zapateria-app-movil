5645dbb7a655e4e278573b78af96cdc0de73ea6f
javier teheran magallanez <141957021+jatemast@users.noreply.github.com>
2025-12-01 11:55:18 -0500

    ya solucionado  error de

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") version "8.1.0"
-    id("org.jetbrains.kotlin.android") version "1.9.0"
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     id("com.google.dagger.hilt.android")
-    kotlin("kapt") version "1.9.0"
+    kotlin("kapt")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,6 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        // Android Gradle Plugin (AGP)
-        classpath("com.android.tools.build:gradle:8.2.2")
-
-        // Plugin de Hilt
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
-    }
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
 
